73b444ad1d02039d71a141bb27ca8ba7e017888d
denis soukh <dsoukh@gmail.com>
2023-03-30 16:25:30 +0200

    añado maqueta a tema wordpress

---
+++ wp-content/themes/curriculum-denis/header.php
@@ -0,0 +1,34 @@
+<!-- header -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Curriculum</title>
+
+    <?php wp_head();?>
+</head>
+<body>
+    <div class="">
+        <header>
+            <nav>
+                <div class="logo">
+                    <h4>Denis Soukharev</h4>
+                </div>
+                <ul class="nav-links">
+                    <li><a href="#perfil">Perfil</a></li>
+                    <li><a href="#competencias">Competencias</a></li>
+                    <li><a href="#experiencia">Experiencia</a></li>
+                    <li><a href="#formacion">Formacion</a></li>
+                    <li><a href="#contactos">Contacto</a></li>
+                </ul>
+                <div class="burger">
+                    <div class="line1"></div>
+                    <div class="line2"></div>
+                    <div class="line3"></div>
+                </div>
+            </nav>
+        </header>
---
+++ wp-content/themes/curriculum-denis/index.php
@@ -0,0 +1,154 @@
+<?php get_header();?>
+
+
+<main>
+            <!-- Hero Section  -->
+            <section class="hero">
+                  <div class="profile" >
+                    <div class="profile-image"><img src="imgs/profile-image.png" alt=""></div>
+                    <div class="profile-details">
+                        <h2>Denis Soukharev</h2>
+                        <h4>Desarollador PHP y Wordpress</h4>
+                        <p><strong> Direccion: </strong> <span>Santander, Cantabria</span></p>
+                        <p><strong> E-mail: </strong><span>dsoukh@gmail.com</span></p>
+                        <p><strong> Telefono: </strong><span>+34642211040</span></p>
+                        <button class="descargar">Descargar en Pdf</button>
+                    </div>
+                  </div>
+                  
+            </section>
+
+
+             <!-- Perfil -->
+             <section class="perfil" id="perfil">
+                <div class="perfil-sumar">
+                <h2>Perfil</h2>
+                <p>Un programmador junior altamente motivado, flexible y trabajador con un conocimiento perfecto de <strong> Ingles, Español y Ruso.</strong> Buscando activamente una oportunidad para adquirir experiencia laboral y aplicar los conocimientos adquiridos hasta ahora.</p>
+               </div>
+             </section>
+
+
+            <!-- Competencias -->
+            <section class="comp" id="competencias">
+                 <h2>Competencias</h2>
+                   <div class="competencias-cajas">
+                     <div class="competencias-caja">
+                        <img src="imgs/php.png" alt="">
+                         <h6>PHP</h6>
+                         <p>Desarolle varios proyectos utilizando PHP</p>
+                     </div>
+                     <div class="competencias-caja">
+                        <img src="imgs/html-5 (1).png" alt="">
+                         <h6>PHP</h6>
+                         <p>Desarolle varios proyectos utilizando PHP</p>
+                     </div>
+                     <div class="competencias-caja">
+                        <img src="imgs/css-3.png" alt="">
+                         <h6>PHP</h6>
+                         <p>Desarolle varios proyectos utilizando PHP</p>
+                     </div>
+                     <div class="competencias-caja">
+                        <img src="imgs/wordpress.png" alt="">
+                         <h6>PHP</h6>
+                         <p>Desarolle varios proyectos utilizando PHP</p>
+                     </div>
+                   </div>
+                   <!-- Baras de progreso  -->
+                   <div class="progreso">
+                    <div class="progreso-left">
+                        <h6>Javascript</h6>
+                        <div class="progress">
+                            <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                        <h6>Javascript</h6>
+                        <div class="progress">
+                            <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                    </div>
+                    <div class="progreso-right">
+                        <h6>Yii</h6>
+                        <div class="progress">
+                            <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                        <h6>Yii</h6>
+                        <div class="progress">
+                            <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                    </div>
+                   </div>
+                  
+
+            </section>
+
+
+              <!-- Experiencia -->
+              <section class="experiencia" id="experiencia">
+                <h2>Experiencia</h2>
+
+                <div class="experiencia-cajas">
+                    <div class="experiencia-caja">
+                         <p><strong>enero 2023</strong></p>
+                         <p>Practicas</p>
+                         <p><span>CeinMark, Santander</span></p>
+                         <p>Desarollo de aplicaciones para administrar processos internos de empresa
+                            (administracion y base de datos de de portatiles)
+                            PHP-Yii2-MySql-Javascript-Html-CSS</p>
+                    </div>
+                    <div class="experiencia-caja">
+                         <p><strong>ene 2020 - nov 2022</strong></p>
+                         <p>Desarollador Web</p>
+                         <p><span>Eksprovocator, Remoto(Estados Unidos)</span></p>
+                         <p>Diseño y desarollo de pagina web con tienda online(Wordpress, Html, Css, PHP, Woocommerce, Stripe)
+                            Desarollo de Temas,Plugins personalizados (HTML,CSS,JS,PHP,Api)
+                            Maquetacion Web (HTML, CSS , Elementor,PHP)
+                            Mantenimiento Web</p>
+                    </div>
+                </div>
+
+
+
+            </section>
+
+
+
+
+            <!-- Formacion -->
+            <section class="formacion" id="formacion">
+                <h2>Formacion</h2>
+
+                <div class="formacion-cajas">
+                    <div class="formacion-caja">
+                        <p><strong>sep 2015 - jul 2017</strong></p>
+                        <p>Bachillerato</p>
+                        <p><span>Laude British School of Villa Real, Villa Real</span></p>
+                    </div>
+                    <div class="formacion-caja">
+                        <p><strong>sep 2018 - jul 2020</strong></p>
+                        <p>Ingenieria Electrica</p>
+                        <p><span>Universitat Jaume 1, Castellon-de-la-Plana</span></p>
+                    </div>
+                    <div class="formacion-caja">
+                        <p><strong>jun 2022 - dic 2022</strong></p>
+                        <p>Programación con lenguajes orientados a objetos y bases de datos</p>
+                        <p><span>Alpe Formacion, Santander</span></p>
+                    </div>
+                    <div class="formacion-caja">
+                        <p><strong>sep 2021 - dic 2021</strong></p>
+                        <p>The Modern Javascript Bootcamp Course</p>
+                        <p><span>Udemy, Online</span></p>
+                    </div>
+                </div>
+
+
+
+            </section>
+
+            <!-- Contactos -->
+
+            <section class="contactos" id="contactos">
+
+            </section>
+        </main>
+
+
+        <?php get_footer();?>
