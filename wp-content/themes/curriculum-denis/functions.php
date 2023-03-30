3269056da6daa01026e531790ba16a9bb924ee81
denis soukh <dsoukh@gmail.com>
2023-03-30 16:49:26 +0200

    añadido script a functions.php

--- wp-content/themes/curriculum-denis/functions.php
+++ wp-content/themes/curriculum-denis/functions.php
@@ -5,3 +5,13 @@ function add_css()
    wp_enqueue_style( 'first');
 }
 add_action('wp_enqueue_scripts', 'add_css');
+
+
+function add_script()
+{
+   wp_register_script('navbar', get_template_directory_uri() . '/assets/navbar.js', false, 1.1, true);
+   wp_enqueue_script( 'navbar');
+
+}
+
+add_action('wp_enqueue_scripts', 'add_script');
